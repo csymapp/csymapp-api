ab84e5c7456860bc792d5a57b2eefc3c65cb644c
Brian Onang'o <surgbc@gmail.com>
2018-12-10 14:36:58 +0300

    Restart on update

--- csymapp-api.update.sh
+++ csymapp-api.update.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 readConfig () {
-	set -a 
-	source .env
-	set +a 
+        set -a 
+        source .env
+        set +a 
 }
 
 readConfig
 
-checktime=$UPDATE_INTERVAL #1 hour	 #time in minutes after which to check for new updates
+checktime=$UPDATE_INTERVAL #1 hour       #time in minutes after which to check for new updates
 
 #times in seconds
 checktime=$((checktime * 60))
 
+pullUpdates() {
+        (git pull origin master | grep "Already up-to-date") && echo "Nothing more to do " || systemctl restart csymapp-api
+}
 
-git pull origin master
+pullUpdates
+#git pull origin master
 
-while :			#continuously check for internet
+while :
 do
    {
-   		sleep $checktime
-   		git pull origin master
-   }
+		sleep $checktime
+		pullUpdates
+	}
 done
